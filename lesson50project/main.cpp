b4253041dd412ed97c24ba07a553a2dad6c2bd91
tttttttteemmaa <malakhovskiyartem@gmail.com>
2023-03-15 17:40:00 +0300

    add student class definition and small logic

--- lesson50project/main.cpp
+++ lesson50project/main.cpp
@@ -1,6 +1,35 @@
 #include "main.h"
 
+class Student {
+public:
+	string name;
+	string surname;
+	int age;
+	float avg_mark;
+};
+
 int main() {
+	Student student1;
+	Student student2;
+
+	student1.name = "Ivan";
+	student1.surname = "Ivanov";
+	student1.avg_mark = 10;
+	student1.age = 15;
+
+	student2.name = "Peter";
+	student2.surname = "Petrov";
+	student2.age = 14;
+	student2.avg_mark = 4;
+
+	if (student1.avg_mark > student2.avg_mark) {
+		cout << student1.name << " " << student1.surname <<" is the best student.";
+
+	}
+	else {
+		cout << student2.name << " " << student2.surname << " is the best student.";
+	}
+	
 
 	return 0;
 }
